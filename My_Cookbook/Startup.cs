b9d5f8e0a1b94455b099415ea9f1ab7a675ca24c
MWight20 <mwight20@gmail.com>
2018-10-19 10:11:45 -0500

    Add project files.

---
+++ My_Cookbook/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(My_Cookbook.Startup))]
+namespace My_Cookbook
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
